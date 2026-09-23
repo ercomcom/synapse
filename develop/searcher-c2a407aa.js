c3a00d6c067effda057bbefddc8db1436b13f955
ASR-ASU <ASR-ASU@users.noreply.github.com>
2026-09-23 14:11:40 +0000

    deploy: 818f2b25bbd78e3754c924c969c3fa5d94ab67c8

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e44a9d4b.js',
+                path_to_root + 'searchindex-aeb8274f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
